0a7d554fa37b184b17cd58cb11476dd09a1019d6
Arham Ahmed <a.arhamIT@gmail.com>
2015-05-10 17:08:29 -0400

    added mongo integration
    -user data is now stored on mongo
    -length error on jade

--- userDatabase/routes/index.js
+++ userDatabase/routes/index.js
@@ -6,4 +6,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET Hello World page */
+router.get('/helloworld', function(req, res) {
+	res.render('helloworld', {title: 'Hello, World!'})
+});
+
+/* GET UserList page */
+//extracts 'db' object in http request to fill 'docs' var with
+//database documents
+router.get('/userlist', function(req, res){
+	var db = req.db;
+	var collection = db.get('usercollection');
+	collection.find({}, {}, function(e, docs){
+		res.render('userlist', {
+			"userlist" : docs
+		});
+	});
+});
+
 module.exports = router;
